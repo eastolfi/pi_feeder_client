97663dc740571d43e3785fd6e92ca2eb2ac0bceb
eastolfi <astolfi_wotc@hotmail.com>
2017-05-04 06:38:37 +0000

    feat: Create repository

---
+++ app/main_mobile.ts
@@ -0,0 +1,4 @@
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { AppModule } from "./pages/mobile/core/app.module";
+
+platformNativeScriptDynamic().bootstrapModule(AppModule);
